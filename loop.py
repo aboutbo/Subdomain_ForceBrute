d28d9f7b7c84a11f914ea4f9594149af8de12c2e
aboutbo <fantasyxubo@gmail.com>
2019-04-30 16:47:03 +0800

    fix somes bugs

--- loop.py
+++ loop.py
@@ -3,22 +3,25 @@ from query import domain_query
 # 字符集
 char_set = "0123456789abcdefghijklmnopqrstuvwxyz"
 # 主域名
-domain = "iqiyi.com"
+domain = "cheyipai.com"
 # 子域名长度
 SUB_DM_LEN = 3
 
 
 # 使用递归遍历子域名
-def travel(length, prefix):
+# todo:使用装饰器遍历查询
+
+def travel(length, prefix, wildcard):
     for string in char_set:
         if length > len(string):
             new_prefix = prefix + string
-            travel(length - len(string), new_prefix)
+            travel(length - len(string), new_prefix, wildcard)
         sub_str = prefix + string
         if len(sub_str) == SUB_DM_LEN:
             # print(sub_str)
-            domain_query(sub_str + '.' + domain)
+            domain_query(sub_str + '.' + domain, wildcard)
 
 
 if __name__ == "__main__":
-    travel(SUB_DM_LEN, '')
+    wildcard_ip = "210.74.2.227"
+    travel(SUB_DM_LEN, '', wildcard_ip)
--- query.py
+++ query.py
@@ -3,6 +3,13 @@ import dns.resolver
 import dns.exception
 import functools
 
+# 字符集
+char_set = "0123456789abcdefghijklmnopqrstuvwxyz"
+# 主域名
+DOMAIN = "cheyipai.com"
+# 子域名长度
+SUB_DM_LEN = 3
+
 
 # 域名查询功能装饰器
 def query(func):
@@ -11,6 +18,10 @@ def query(func):
         try:
             answer = dns.resolver.query(args[0], rdtype='A')
             # print(answer)
+            if len(args) > 1:
+                return func(domain=args[0], answer=answer, wildcard_ip=args[1])
+            else:
+                return func(domain=args[0], answer=answer)
         except dns.exception.Timeout:
             print("查询超时")
         except dns.resolver.NXDOMAIN:
@@ -18,14 +29,15 @@ def query(func):
             # print("没有对应A记录")
         except (dns.resolver.YXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
             print("查询异常")
-        if len(args)>1:
-            return func(domain=args[0], answer=answer, wildcard_ip=args[1])
-        else:
-            return func(domain=args[0], answer=answer)
+        # if len(args) > 1:
+        #     return func(domain=args[0], answer=answer, wildcard_ip=args[1])
+        # else:
+        #     return func(domain=args[0], answer=answer)
 
     return wrapper
 
 
+# 遍历输出一个子域名的A记录查询结果
 @query
 def domain_query(domain, answer, wildcard_ip):
     if not wildcard_ip:
@@ -37,6 +49,7 @@ def domain_query(domain, answer, wildcard_ip):
             print(record.address, end='')
 
 
+# 判断域名通配符解析wildcard records是否存在
 @query
 def wildcard_dns_records(domain, answer):
     if answer:
@@ -48,6 +61,20 @@ def wildcard_dns_records(domain, answer):
         print("不存在DNS泛解析")
 
 
+# 使用递归遍历子域名
+def travel(length, prefix, wildcard):
+    for string in char_set:
+        if length > len(string):
+            new_prefix = prefix + string
+            travel(length - len(string), new_prefix, wildcard)
+        sub_str = prefix + string
+        if len(sub_str) == SUB_DM_LEN:
+            # print(sub_str)
+            domain_query(sub_str + '.' + DOMAIN, wildcard)
+
+
 if __name__ == "__main__":
     ip = wildcard_dns_records("xubo.cheyipai.com")
     domain_query("123.cheyipai.com", ip)
+    # wildcard_ip = "210.74.2.227"
+    travel(SUB_DM_LEN, '', ip)
